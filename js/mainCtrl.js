62db8e970b0294e8e9d3400a94bf9b48f4c01c83
Stephen Done <srdone@gmail.com>
2015-02-10 20:48:57 -0700

    added filter

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -4,13 +4,18 @@ app.controller('mainCtrl', function($scope, itunesService){
   //This is setting up the default behavior of our ng-grid. The important thing to note is
   //the 'data' property. The value is 'songData'. That means ng-grid is looking for songData on $scope and is putting whatever songData is into the grid.
   //this means when you make your iTunes request, you'll need to get back the information, parse it accordingly, then set it to songData on the scope -> $scope.songData = ...
+  $scope.filterOptions = {
+    filterText: ''
+  };
+
   $scope.gridOptions = { 
       data: 'songData',
       height: '110px',
+      filterOptions: $scope.filterOptions,
       sortInfo: {fields: ['Song', 'Artist', 'Collection', 'Type'], directions: ['asc']},
       columnDefs: [
         {field: 'Play', displayName: 'Play', width: '40px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><a href="{{row.getProperty(col.field)}}"><img src="http://www.icty.org/x/image/Miscellaneous/play_icon30x30.png"></a></div>'},
-        {field: 'Artist', displayName: 'Artist'},
+        {field: 'Artist', displayName: 'Artist', filter: {term: 'Artist'}},
         {field: 'Collection', displayName: 'Collection'},
         {field: 'AlbumArt', displayName: 'Album Art', width: '110px', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><img src="{{row.getProperty(col.field)}}"></div>'},
         {field: 'Type', displayName: 'Type'},
@@ -46,6 +51,15 @@ app.controller('mainCtrl', function($scope, itunesService){
         $scope.songData = data;
       })
     };
+
+    $scope.searchOptions = [
+      {value: 'all', displayText: 'All'},
+      {value: 'musicVideo', displayText: 'Music Video'},
+      {value: 'movie', displayText: 'Movie'},
+      {value: 'musicTrack', displayText: 'Music Track'},
+      {value: 'podcast', displayText: 'Podcast'},
+      {value: 'tvShow', displayText: 'TV Show'},
+    ];
 
   //Check that the above method is working by entering a name into the input field on your web app, and then console.log the result
 
